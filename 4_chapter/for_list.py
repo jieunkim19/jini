f06f48089341c2f57df82bbe64ae344defa5a058
jini <jieunkim@hanbit.co.kr>
2020-03-22 16:40:56 +0900

    반복문

---
+++ 4_chapter/for_list.py
@@ -0,0 +1,6 @@
+# 리스트 선언
+array = [11, 22, 33, 44, 55]
+
+# 리스트에 반복문 적용
+for element in array:
+    print(element)
---
+++ 4_chapter/list03.py
@@ -0,0 +1,12 @@
+# 리스트 선언
+list_a = [1,2,3]
+print("# 리스트의 요소 하나 제거")
+
+# del로 제거
+del list_a[0]
+print("del list_a[0]:", list_a)
+
+# pop()로 제거
+list_a.pop(1)
+print("pop(1):", list_a)
+
